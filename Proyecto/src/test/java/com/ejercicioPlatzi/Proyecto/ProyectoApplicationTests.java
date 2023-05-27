797d90fe02bc04997f85b13707f1933f823ab1cf
Tu nombre <andres.molina9007@gmail.com>
2023-05-27 15:53:03 -0500

    first commit

---
+++ Proyecto/src/test/java/com/ejercicioPlatzi/Proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicioPlatzi.Proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
